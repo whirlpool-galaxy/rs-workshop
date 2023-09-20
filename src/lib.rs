a5ea9bd1cf8f5f882cd889c38e157b74db106093
Jonathan "Nath" Schild <nath@j-schild.de>
2023-09-20 18:53:18 +0200

    Add schduler skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod raw_tables;
+pub mod scheduler;
---
+++ src/scheduler.rs
@@ -0,0 +1,38 @@
+pub type ParticipantID = i32;
+pub type WorkshopID = i32;
+pub type TimeslotID = i32;
+pub type RoomID = i32;
+pub type Priority = i32;
+
+pub trait ParticipantQueue<P: Participant> {
+    fn dequeue(&mut self) -> P;
+    fn is_empty(&self) -> bool;
+}
+
+pub trait Participant {
+    fn get_priority_for(&self, workshop_id: WorkshopID) -> Priority;
+    fn schedule(&mut self, schedule: &Vec<(TimeslotID, WorkshopID)>);
+}
+
+pub trait Rooms {
+    fn schedule_participant_to(&mut self, schedule: &Vec<(TimeslotID, WorkshopID)>) -> bool;
+    fn get_available_wt(&self) -> Vec<(TimeslotID, WorkshopID)>;
+}
+
+pub fn schedule(participants: &mut dyn ParticipantQueue<impl Participant>, rooms: &mut dyn Rooms) {
+    // get_available_wt
+    while !participants.is_empty() {
+        let mut p = participants.dequeue();
+
+        loop {
+            // optimise priority score
+            let schedule = vec![(0, 0)];
+            if rooms.schedule_participant_to(&schedule) {
+                p.schedule(&schedule);
+                break;
+            } else {
+                // update get_available_wt
+            }
+        }
+    }
+}
