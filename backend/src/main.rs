3917ff801aad525bf246d686518ca73c5ba1e709
Jonathan "Nath" Schild <nath@j-schild.de>
2023-09-30 00:25:41 +0200

    Add db pool to server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,28 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web::Data, App, HttpServer};
 use api::{admin::admin_config, participant::participant_config};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 mod api;
 
+struct DataBase {
+    pub db: Pool<Postgres>,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let db_pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://rs-workshop:rs-workshop@localhost/rs-workshop")
+        .await
+        .expect("Cannot connect to Database!");
+
+    HttpServer::new(move || {
         App::new()
             .configure(admin_config)
             .configure(participant_config)
+            .app_data(Data::new(DataBase {
+                db: db_pool.clone(),
+            }))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
